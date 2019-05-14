705e4c8da0e812e8914ccd613da69a957d2f66f6
superman <supermanchc@gmail.com>
2019-05-14 10:13:18 +0800

    fix(Server 酱推送 api 更换为配置文件读取):

--- src/service/ServerPushService.ts
+++ src/service/ServerPushService.ts
@@ -14,7 +14,7 @@ class ServerPushService {
    */
   sendMessageToWechat(text: string, desp?: string) {
     if (typeof this.key !== 'undefined') {
-      Axios.get(apis.push, {
+      Axios.get(`${apis.push***REMOVED***.${this.key***REMOVED***.send`, {
         params: {
           text,
           desp
